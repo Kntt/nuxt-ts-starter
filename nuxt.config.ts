50ec485ae22d6490fb82d6a3faf3e9a1a34f8829
Kntt <kntt9968@gmail.com>
2019-02-23 16:22:49 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+export default {
+    env: {
+        baseUrl: process.env.BASE_URL || `http://localhost:3000`
+    },
+    head: {
+        title: 'nuxt-ts-template',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Nuxt.js project'
+            }
+        ],
+        link: [
+            {
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico'
+            }
+        ]
+    },
+    /*
+   ** Customize the progress-bar color
+   */
+    loading: { color: '#3B8070' },
+    /*
+   ** Build configuration
+   */
+    css: ['~/assets/css/main.css'],
+    srcDir: './src',
+    build: {
+        extend (config: any, ctx: any) {
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(ts|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
+    },
+    modules: ['@nuxtjs/axios'],
+    axios: {}
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class HelloWorld extends Vue {
+    message: string = 'Hello world ! typescript'
+}
+</script>
