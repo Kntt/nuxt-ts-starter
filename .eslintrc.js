bd4891f407830a6d6817e02c355680611c9dcad2
Kntt <kntt9968@gmail.com>
2019-02-25 23:21:23 +0800

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,14 @@ module.exports = {
     'standard',
     'plugin:@typescript-eslint/recommended'
   ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-undef': 'off',
     'import/no-duplicates': 'off'
-  },
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
   }
 }
